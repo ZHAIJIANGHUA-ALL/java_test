b0ee6f94ffd538ee562047a9d3828995fb0e6dfe
admin <13939766137@163.com>
2019-06-27 17:25:35 +0800

    添加到本地

---
+++ java_test/src/cn/zjh/class1.java
@@ -0,0 +1,10 @@
+package cn.zjh;
+
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("====");
+        System.out.println("222222222");
+        System.out.println("3333333333");
+        System.out.println("444444444444");
+    }
+}
